1216a270a4fa9862290b64f1c0cc415504696c1b
sanpwintthu <sanwpwintthu9@gmail.com>
2018-05-25 19:36:51 +0630

    bootstrap cdn fix

--- resources/views/bp-admin/layouts/app.blade.php
+++ resources/views/bp-admin/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <!-- Fonts -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
     <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" type="text/css" href="{{ asset("css/app.css") }}">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 
     <!-- Styles -->
     
